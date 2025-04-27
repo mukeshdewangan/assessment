863f60165fcfb68e1704abadb22aabc4778b3234
Mukesh Dewangan <mdewangan@salesforce.com>
2025-04-27 16:45:05 +0530

    metric logger

--- CircuitBreaker/src/main/java/com/assessment/mukesh/circuitbreaker/MetricLogger.java
+++ CircuitBreaker/src/main/java/com/assessment/mukesh/circuitbreaker/MetricLogger.java
@@ -1,2 +1,18 @@
-package com.assessment.mukesh.circuitbreaker;public class MetricLogger {
+package com.assessment.mukesh.circuitbreaker;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class MetricLogger {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private static final LogHelper log = LogHelper.getLogger(MetricLogger.class);
+
+    public void logCircuitBreakerMetrics(CircuitBreakerMetric metrics) {
+        try {
+            String serializedMetrics = objectMapper.writeValueAsString(metrics);
+            log.info(serializedMetrics);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
 }

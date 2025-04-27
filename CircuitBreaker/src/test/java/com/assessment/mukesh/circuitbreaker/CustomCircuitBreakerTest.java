f676a7b8b2c3e8d4ba99438b5874f7fa1bd6b6dc
Mukesh Dewangan <mdewangan@salesforce.com>
2025-04-27 15:03:42 +0530

    renaming files

---
+++ CircuitBreaker/src/test/java/com/assessment/mukesh/circuitbreaker/CustomCircuitBreakerTest.java
@@ -0,0 +1,64 @@
+package com.assessment.mukesh.circuitbreaker;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class CustomCircuitBreakerTest {
+
+    private CustomCircuitBreaker circuitBreaker;
+
+    @BeforeEach
+    void setUp() {
+        circuitBreaker = new CustomCircuitBreaker(3, 2000);;
+    }
+
+    @Test
+    public void testAllowRequestInitiallyClosed() {
+        assertTrue(circuitBreaker.allowRequest());
+    }
+
+    @Test
+    public void testCircuitOpensAfterFailures() {
+        // Arrange
+        circuitBreaker.recordFailure();
+        circuitBreaker.recordFailure();
+        // Act and Assert
+        assertTrue(circuitBreaker.allowRequest());
+
+        circuitBreaker.recordFailure();
+
+        assertFalse(circuitBreaker.allowRequest());
+    }
+
+    @Test
+    public void testCircuitHalfOpenAfterRetryTime() throws InterruptedException {
+        circuitBreaker.recordFailure();
+        circuitBreaker.recordFailure();
+        circuitBreaker.recordFailure(); // Opens the circuit
+
+        assertFalse(circuitBreaker.allowRequest());
+
+        Thread.sleep(2100); // Sleep longer than retryTimePeriod
+
+        assertTrue(circuitBreaker.allowRequest()) ;
+    }
+
+    @Test
+    public void testCircuitClosesAfterSuccess() throws InterruptedException {
+        circuitBreaker.recordFailure();
+        circuitBreaker.recordFailure();
+        circuitBreaker.recordFailure(); // Open
+
+        Thread.sleep(2100); // wait for half-open
+
+        assertTrue(circuitBreaker.allowRequest());
+
+        circuitBreaker.recordSuccess();
+
+        assertTrue(circuitBreaker.allowRequest());
+    }
+}
+
